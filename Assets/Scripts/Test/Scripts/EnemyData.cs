5239484243517a532ebc0dab170e235713dd6883
Kim Seongho <yrs03143@gmail.com>
2025-06-24 17:12:50 +0900

    ScriptableObject Test
    타워클래스 작성 전 ScriptableObject 샘플 테스트
    - EnemyData
    - Enemy
    - EnemySpawner
    - EnemtTestManager

---
+++ Assets/Scripts/Test/Scripts/EnemyData.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyData", menuName = "Test/EnemyData")]
+public class EnemyData : ScriptableObject
+{
+    [Header("적 정보")]
+    public string name;  
+    public float maxHp;       
+    public float speed;       
+
+    [Header("프리팹 주소 (Addressable")]
+    public string prefabAddress;
+
+    [Header("프리팹 직접 참조")]
+    public GameObject prefab;
+}
---
+++ Assets/Scripts/Test/Scripts/EnemySpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+// Addressable 기반으로 적 생성
+public class EnemySpawner : MonoBehaviour
+{
+    [Header("스폰할 적 데이터")]
+    public EnemyData[] enemyDatas;
+
+    public void SpawnEnemy(int index)
+    {
+        if (index < 0 || index >= enemyDatas.Length) return;
+        EnemyData enemyData = enemyDatas[index];
+
+        // Addressable 사용시
+        //StartCoroutine(SpawnRoutine(enemyData));
+
+        // 프리팹 직접 참조 후 인스턴스화
+        GameObject go = Instantiate(enemyData.prefab, transform.position, Quaternion.identity);
+        Enemy enemy = go.GetComponent<Enemy>();
+        enemy.Init(enemyData);
+    }
+
+    // Addressable 사용시
+    //private IEnumerator SpawnRoutine(EnemyData enemyData)
+    //{
+    //    AsyncOperationHandle<GameObject> handle = Addressables.LoadAssetAsync<GameObject>(enemyData.prefabAddress);
+    //    yield return handle;
+
+    //    if (handle.Status == AsyncOperationStatus.Succeeded)
+    //    {
+    //        GameObject go = Instantiate(handle.Result, transform.position, Quaternion.identity);
+    //        Enemy enemy = go.GetComponent<Enemy>();
+    //        enemy.Init(enemyData);
+    //    }
+    //    else
+    //    {
+    //        Debug.LogError("프리팹 로드 실패: " + enemyData.prefabAddress);
+    //    }
+    //}
+}
