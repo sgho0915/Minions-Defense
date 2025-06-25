adc541aa39c7411327496d942ca08f26106db218
Kim Seongho <yrs03143@gmail.com>
2025-06-25 21:13:51 +0900

    [Fix] 타워 열거형 스크립트 통합 및 툴팁 주석화

---
+++ Assets/Scripts/Towers/Enums/TowerEnums.cs
@@ -0,0 +1,32 @@
+﻿// TowerEnums.cs
+/// <summary>
+/// 타워 구분용 타입 열거형
+/// </summary>
+public enum TowerType
+{
+    Mage, Cannon, DogBarrack, MachineGun, Main
+}
+
+/// <summary>
+/// 4단계에서 A/B 타입 구분을 위한 열거형
+/// </summary>
+public enum TowerLevel4Type
+{
+    None, A, B
+}
+
+/// <summary>
+/// 타겟 우선순위 옵션
+/// </summary>
+public enum TowerTargetPriority
+{
+    First, Last, Strongest, Weakest, Closest
+}
+
+/// <summary>
+/// AoE 광역 공격 효과 형태
+/// </summary>
+public enum TowerAreaShape
+{
+    None, Circle, Cone, Line
+}
