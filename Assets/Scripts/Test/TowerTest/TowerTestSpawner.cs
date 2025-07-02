c8b08f3cb7cbc3132aa9d9822945e06515c7cfb0
Kim Seongho <yrs03143@gmail.com>
2025-07-02 22:12:55 +0900

    [Test] TowerTestSpawner 타워 스폰 테스트 호출 스크립트 작성

---
+++ Assets/Scripts/Test/TowerTest/TowerTestSpawner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class TowerTestSpawner : MonoBehaviour
+{
+    public TowerDataSO canonData;
+    public Transform spawnPoint;
+    private TowerController lastTower;
+
+    public void SpawnLevel1Tower()
+    {
+        // levelData 배열의 첫 요소로 소환
+        var go = TowerFactory.Instance
+                    .CreateTower(canonData.levelData[0], canonData.levelData, spawnPoint.position, null);
+        lastTower = go.GetComponent<TowerController>();
+    }
+}
