5d3310844dc2a9623f8e7456947661f55a9fb0b1
Kim Seongho <yrs03143@gmail.com>
2025-07-04 17:37:45 +0900

    [Test] TestMonster SO 생성 및 버튼으로 테스트 필드로 스폰

--- Assets/Scripts/Test/TowerTest/TowerTestSpawner.cs
+++ Assets/Scripts/Test/TowerTest/TowerTestSpawner.cs
@@ -1,16 +1,28 @@
-﻿using UnityEngine;
+﻿using System.Drawing;
+using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class TowerTestSpawner : MonoBehaviour
 {
     public TowerDataSO canonData;
-    public Transform spawnPoint;
+    public MonsterDataSO monsterData;
+    public Transform towerSpawnPoint;
+    public Transform monsterSpawnPoint;
     private TowerController lastTower;
+    private MonsterController monsterController;
 
     public void SpawnLevel1Tower()
     {
         // levelData 배열의 첫 요소로 소환
         var go = TowerFactory.Instance
-                    .CreateTower(canonData.levelData[0], canonData.levelData, spawnPoint.position, null);
+                    .CreateTower(canonData.levelData[0], canonData.levelData, towerSpawnPoint.position, null);
         lastTower = go.GetComponent<TowerController>();
+    }
+
+    public void SpawnMonster()
+    {
+        // levelData 배열의 첫 요소로 소환
+        var go = MonsterFactory.Instance.CreateMonster(monsterData, MonsterSize.Small, monsterSpawnPoint.position, null);
+        monsterController = go.GetComponent<MonsterController>();
     }
 }
