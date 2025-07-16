904035083643c7e913cd9c3d339843caecfd1536
Kim Seongho <yrs03143@gmail.com>
2025-07-16 20:26:12 +0900

    [Feature] 메인타워 체력 관련 MainTowerController, MainTowerView 추가

---
+++ Assets/Scripts/Towers/View/MainTowerView.cs
@@ -0,0 +1,45 @@
+﻿// MainTowerView.cs
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// MainTowerController 전용 View
+/// </summary>
+public class MainTowerView : MonoBehaviour
+{
+    [SerializeField] Slider hpSlider;
+    private CanvasGroup canvasGroup;
+    private MainTowerController mainTowerController;
+
+    public void Initialize(MainTowerController mainTower)
+    {
+        mainTowerController = mainTower;
+        canvasGroup = GetComponent<CanvasGroup>();
+        hpSlider.maxValue = mainTowerController.MaxHp;
+        hpSlider.value = mainTowerController.CurrentHp;
+        canvasGroup.alpha = 1;
+
+        mainTowerController.OnHpChanged += UpdateHpBar;
+        mainTowerController.OnDied += HideHpBar;
+    }
+
+    private void UpdateHpBar(int curHp, int max)
+    {
+        hpSlider.value = curHp;
+    }
+
+    private void HideHpBar()
+    {
+        canvasGroup.DOFade(0, 3);
+    }
+
+    private void OnDestroy()
+    {
+        if (mainTowerController != null)
+        {
+            mainTowerController.OnHpChanged -= UpdateHpBar;
+            mainTowerController.OnDied -= HideHpBar;
+        }
+    }
+}
