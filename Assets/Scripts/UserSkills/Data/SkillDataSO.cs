8bd2768bb5f8a96fca4174c7b9b16a07c4a73bb6
Kim Seongho <yrs03143@gmail.com>
2025-08-26 21:16:52 +0900

    [Feature] SkillDataSO : 스킬데이터 공통 사용 및 컨트롤러 생성 책임

---
+++ Assets/Scripts/UserSkills/Data/SkillDataSO.cs
@@ -0,0 +1,20 @@
+﻿// SkillDataSO.cs
+using UnityEngine;
+
+/// <summary>
+/// 모든 스킬 데이터 SO의 기반이 되는 추상 클래스
+/// UI 표시에 필요한 공통 정보, 자신을 제어할 Controller를 생성하는 책임을 가짐
+/// 추상 메서드를 가지고 있으므로 SkillDataSO도 추상 클래스가 됨
+/// </summary>
+public abstract class SkillDataSO : ScriptableObject
+{
+    [Header("기본 정보")]
+    public string skillName;
+
+    /// <summary>
+    /// 해당 스킬 데이터를 제어할 스킬 컨트롤러를 생성하고 반환함
+    /// </summary>
+    /// <param name="owner">스킬 주체</param>
+    /// <returns>생성된 스킬 컨트롤러</returns>
+    public abstract ISkill CreateSkill(Transform owner);
+}
