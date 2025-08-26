7c778d5d9bccb0d6975d59f20c9bc0fe99f6a4fe
Kim Seongho <yrs03143@gmail.com>
2025-08-26 21:21:27 +0900

    [Update] TowerSkillSelectionController : 스킬 관련 이벤트 구독 및 처리 로직 추가

--- Assets/Scripts/UI/Stage/TowerSkillView/TowerSkillSelectionController.cs
+++ Assets/Scripts/UI/Stage/TowerSkillView/TowerSkillSelectionController.cs
@@ -6,26 +6,52 @@ using UnityEngine;
 /// </summary>
 public class TowerSkillSelectionController : MonoBehaviour
 {
+    [Header("Views")]
     [SerializeField] TowerSkillListView listView;
-    [SerializeField] TowerInfoView infoView;
+    [SerializeField] TowerInfoView towerInfoView;
+    [SerializeField] SkillInfoView skillInfoView;
+
+    [Header("Data")]
     [SerializeField] TowerDataSO[] allTowers;
+    [SerializeField] SkillDataSO[] allSkills;
+
+    private System.Collections.Generic.List<ISkill> _activeSkills = new();  // 생성된 스킬 컨트롤러 인스턴스 저장
 
     private void Awake()
     {
+        // 타워 이벤트 구독
         listView.OnTowerSelected += HandleTowerSelected;
-        infoView.OnBuyClicked += HandleTowerBuy;
+        towerInfoView.OnBuyClicked += HandleTowerBuy;
+
+        // 스킬 이벤트 구독
+        listView.OnSkillSelected += HandleSkillSelected;
+        skillInfoView.OnExecuteClicked += HandleSkillExecute;
+        skillInfoView.OnUpgradeClicked += HandleSkillUpgrade;
     }
 
     private void Start()
     {
-        listView.Populate(allTowers);
-        infoView.Hide();
+        listView.PopulateTowers(allTowers);
+
+        foreach (var skillSO in allSkills)
+        {
+            ISkill skillInstance = skillSO.CreateSkill(this.transform);
+            _activeSkills.Add(skillInstance);
+        }
+        listView.PopulateSkills(_activeSkills);
+
+        towerInfoView.Hide();
+        skillInfoView.Hide();
     }
 
     private void OnDestroy()
     {
         listView.OnTowerSelected -= HandleTowerSelected;
-        infoView.OnBuyClicked -= HandleTowerBuy;
+        towerInfoView.OnBuyClicked -= HandleTowerBuy;
+
+        listView.OnSkillSelected -= HandleSkillSelected;
+        skillInfoView.OnExecuteClicked -= HandleSkillExecute;
+        skillInfoView.OnUpgradeClicked -= HandleSkillUpgrade;
     }
 
     /// <summary>
@@ -34,13 +60,39 @@ public class TowerSkillSelectionController : MonoBehaviour
     /// <param name="towerDataSO"></param>
     private void HandleTowerSelected(TowerDataSO towerDataSO)
     {
-        infoView.ShowTowerBuyView(towerDataSO);
+        towerInfoView.ShowTowerBuyView(towerDataSO);
+        skillInfoView.Hide();
     }
 
     private void HandleTowerBuy(TowerDataSO towerDataSO)
     {
         // 레벨1 기준으로 배치 시작
         TowerPlacementController.Instance.BeginPlacement(towerDataSO, towerDataSO.levelData[0]);
-        infoView.Hide();
+        towerInfoView.Hide();
+    }
+
+    private void HandleSkillSelected(ISkill skill)
+    {
+        towerInfoView.Hide();
+        skillInfoView.Show(skill);
+    }
+
+    private void HandleSkillExecute(ISkill skill)
+    {
+        int cost = skill.CurrentLevelData.executeCost;
+        if (GameManager.Instance.TrySpendStagePoints(cost))
+        {
+            skill.ExecuteSkill(Vector3.zero);
+            skillInfoView.Hide();
+        }
+        else
+        {
+            Debug.Log("스테이지 포인트 부족");
+        }
+    }
+
+    private void HandleSkillUpgrade(ISkill skill)
+    {
+        // 다음레벨 데이터 확인, 비용차감, skill.setlevel 호출 등 강화 로직
     }
 }
